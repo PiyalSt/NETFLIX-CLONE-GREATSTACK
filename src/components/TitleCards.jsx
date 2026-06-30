7541b3da72622b68449be92017fd5a4e0e9fec75
mrpiyalst <mrpiyalst@gmail.com>
2026-06-30 21:05:52 +0600

    feat:  add TitleCards-components and design

--- src/components/TitleCards.jsx
+++ src/components/TitleCards.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import cards_data from "../assets/cards/Cards_data";
+import ScrollContainer from "react-indiana-drag-scroll";
 
-const TitleCards = () => {
+const TitleCards = ({ title, category}) => {
   return (
-    <div>
-      titleCards
-    </div>
-  )
-}
+    <>
+      <div className="mt-12 mb-8">
+        <h2 className="mb-2">{title ? title : "Popular on Netflix"}</h2>
+        <ScrollContainer className="flex gap-4 overflow-x-scroll scrollbar-none">
+          {cards_data.map((item, index) => (
+            <div className="relative shrink-0" key={index}>
+              <img
+                className="w-60 rounded-sm cursor-pointer"
+                src={item.image}
+                alt={item.name}
+              />
+              <p className="absolute bottom-2 right-2 font-medium">
+                {item.name}
+              </p>
+            </div>
+          ))}
+        </ScrollContainer>
+      </div>
+    </>
+  );
+};
 
-export default TitleCards
+export default TitleCards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import assets from '../assets/assets'
+import TitleCards from '../components/TitleCards'
 
 const Home = () => {
   return (
@@ -9,12 +10,30 @@ const Home = () => {
         <div className='absolute w-full pl-[6%] bottom-0'>
           <img className='' src={assets.hero_title} alt="hero_title" />
           <p className='max-w-175 text-base mb-5'>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy</p>
+
+          {/* hero buttons */}
           <div className='flex gap-3 mb-12'>
             <button className='inline-flex items-center gap-3 border-none outline-none py-2 px-5 text-base font-semibold bg-white rounded-sm cursor-pointer text-black hover:bg-white/70 duration-300 transition-all'> <img className='w-6' src={assets.play_icon} alt="play_icon" /> Play</button>
             <button className='inline-flex items-center gap-3 border-none outline-none py-2 px-5 text-base font-semibold bg-[#6d6d6eb3]/60 backdrop-blur-md rounded-sm cursor-pointer text-white hover:bg-[#6d6d6e66]/40 duration-300 transition-all'> <img className='w-6' src={assets.info_icon} alt="play_icon" /> More Info</button>
           </div>
+
+          {/* titleCards here */}
+          <div className='w-full'>
+            <TitleCards />
+          </div>
+
         </div>
       </div>
+
+      <div className='pl-[6%]'>
+        <div className='w-full'>
+          <TitleCards title={"Blockbuster Movies"} />
+          <TitleCards title={"Only on Netflix"} />
+          <TitleCards title={"Upcoming"} />
+          <TitleCards title={"Top pics for you"} />
+        </div>
+      </div>
+
     </>
   )
 }
